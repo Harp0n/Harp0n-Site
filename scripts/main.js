329be9fe3c203355dc06beb06ec220c5de546b4c
Paweł <pkubala.1lo@gmail.com>
2020-08-05 20:03:02 +0200

    fix: navBar open/close transition fixed

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,21 @@
 var mobileNav = document.querySelector(".mobile-nav");
 var toggleButton = document.querySelector(".toggle-btn");
 
+mobileNav.style.display = "none"; 
 
 toggleButton.addEventListener("click", function() {
     // mobileNav.style.display = 'block';
     // backdrop.style.display = 'block';
-    mobileNav.classList.toggle("active");
-
+    if(mobileNav.classList.contains("active")) {
+        mobileNav.classList.remove("active"); 
+        setTimeout(function () {
+            mobileNav.style.display = "none"; 
+        }, 300);
+    }
+    else {
+        mobileNav.style.display = "block";
+        setTimeout(function () {
+            mobileNav.classList.add("active");
+        }, 10);
+    }
   });
